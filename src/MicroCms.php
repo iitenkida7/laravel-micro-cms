8553f0c88d7ffc62833eca733d6acf08454184e9
iitenkida7 <t.ishii@teradox.net>
2021-09-10 10:07:29 +0900

    Add paginator

--- src/MicroCms.php
+++ src/MicroCms.php
@@ -3,6 +3,8 @@
 namespace Iitenkida7\MicroCms;
 
 use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 
 class MicroCms
 {
@@ -21,4 +23,35 @@ class MicroCms
         $response = $this->client->schema($schema)->get($queryBuilder);
         return $this->convert->get($response);
     }
+
+    public function getWithPaginate(string $schema, QueryBuilder $queryBuilder): LengthAwarePaginator
+    {
+        // @todo: has bugs
+        $queryBuilder->limit = 1;
+        $queryBuilder->offset = $this->getOffsetOrFail($queryBuilder->limit);
+
+        $response = $this->get($schema, $queryBuilder);
+
+        $paginator = new LengthAwarePaginator(
+            $response['contents'],
+            $response['totalCount'],
+            $response['limit'],
+            $response['offset'],
+        );
+
+        return $paginator;
+    }
+
+    private function getOffsetOrFail(int $limit): int
+    {
+        if (is_numeric(request()->page) && request()->page > 0) {
+            return (request()->page - 1) * $limit;
+        } elseif (request()->page) {
+            abort(404);
+        } else {
+            return 0;
+        }
+    }
+
+
 }
